60ec92796ecd96a18ebf9ad850ff68eaff0f6813
Rebekah Eichberg <rebekaheichberg4@gmail.com>
2026-01-15 13:27:35 +0000

    making pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,14 @@
+import sys
+
+print('arguments', sys.argv)
+
+month = int(sys.argv[1])
+import pandas as pd
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+
+print(f"hello rebekah, month={month}")
+
